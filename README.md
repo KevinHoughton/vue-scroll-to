6d084e1047c4556eccdec688524d39efe213cb06
Piotr Kowalski <piecioshka@gmail.com>
2018-01-14 22:52:11 +0100

    Resolve errors and convert to ES2015

--- README.md
+++ README.md
@@ -2,8 +2,8 @@
 
 # vue-scroll-to
 
-[Vue.js](https://github.com/vuejs/vue) directive.  
-Adds a directive that listens for click events and scrolls to elements.
+[Vue.js](https://github.com/vuejs/vue) directive that listens for click 
+events and scrolls to elements.
 
 ## Install
 
@@ -19,13 +19,14 @@ $ yarn add vue-scroll-to
 
 ## Usage
 
-Please note that it's necessary to use single quotes within the double quotes of the directive when defining the class and padding.
+Please note that it's necessary to use single quotes within the double quotes
+of the directive when defining the class and padding.
 
 ```js
 import Vue from 'vue';
 import vueScrollTo from 'vue-scroll-to';
 
-Vue.use(vueScrollTo);
+Vue.use(vueScrollTo, options);
 ```
 
 ```html
@@ -41,6 +42,19 @@ Vue.use(vueScrollTo);
 ```html
 v-scroll-to="'<element>, <padding-in-px>'"
 ```
+
+Default:
+
+* `padding-in-px`: 0
+
+## Settings
+
+Default `options`:
+
+* `speed` = 500 - Animation speed.
+* `padding` = 0 - You can set default global padding, which will override 
+    when put `padding-in-px` value inline with `v-scroll-to`.
+* `movingFrequency` = 15 = Number of steps of animation.
 
 ## License
 
--- index.js
+++ index.js
@@ -1,164 +1,103 @@
 'use strict';
 
-window.$ = document.querySelectorAll.bind(document);
-
-exports.install = function (Vue) {
-    var _ = exports.utils = {
-        $: function (selector) {
-            return document.querySelector(selector);
-        },
-        on: function (element, events, handler) {
-            if (!(events instanceof Array)) {
-                events = [events];
-            }
-            for (var i = 0; i < events.length; i++) {
-                element.addEventListener(events[i], handler);
-            }
-        },
-        off: function (element, events, handler) {
-            if (!(events instanceof Array)) {
-                events = [events];
-            }
-            for (var i = 0; i < events.length; i++) {
-                element.removeEventListener(events[i], handler);
-            }
-        },
-        cumulativeOffset: function (element) {
-            var top = 0;
-
-            do {
-                top += element.offsetTop || 0;
-                element = element.offsetParent;
-            } while (element);
-
-            return {
-                top: top
-            };
+const _ = {
+    $: function (selector) {
+        return document.querySelector(selector);
+    },
+    on: function ($element, events, handler) {
+        if (!(events instanceof Array)) {
+            events = [events];
         }
-    };
-
-    function handleClick(e) {
-
-        var getScrollTopElement = function (e) {
-            var top = 0;
-
-            while (e.offsetParent != undefined && e.offsetParent != null) {
-                top += e.offsetTop + (e.clientTop != null ? e.clientTop : 0);
-                e = e.offsetParent;
-            }
-
-            return top;
-        };
-
-        var getScrollTopDocument = function () {
-            return document.documentElement.scrollTop + document.body.scrollTop;
-        };
-
-        var smoothScrollTo = function (element, target, duration) {
-            target = Math.round(target);
-            duration = Math.round(duration);
-
-            if (duration < 0) {
-                return Promise.reject('bad duration');
-            }
-
-            if (duration === 0) {
-                element.scrollTop = target;
-                return Promise.resolve();
-            }
-
-            var start_time = Date.now();
-            var end_time = start_time + duration;
-            var start_top = element.scrollTop;
-            var distance = target - start_top;
-
-            // based on http://en.wikipedia.org/wiki/Smoothstep
-            var smooth_step = function (start, end, point) {
-                if (point <= start) {
-                    return 0;
-                }
-                if (point >= end) {
-                    return 1;
-                }
-
-                var x = (point - start) / (end - start); // interpolation
-
-                return x * x * (3 - 2 * x);
-            };
-
-            return new Promise(function (resolve, reject) {
-                // This is to keep track of where the element's scrollTop is
-                // supposed to be, based on what we're doing
-                var previous_top = element.scrollTop;
-
-                // This is like a think function from a game loop
-                var scroll_frame = function () {
-
-                    if (element.scrollTop != previous_top) {
-                        reject('interrupted');
-                        return;
-                    }
-
-                    // set the scrollTop for this frame
-                    var now = Date.now();
-                    var point = smooth_step(start_time, end_time, now);
-                    var frameTop = Math.round(start_top + (distance * point));
-                    element.scrollTop = frameTop;
-
-                    // check if we're done!
-                    if (now >= end_time) {
-                        resolve();
-                        return;
-                    }
-
-                    if (element.scrollTop === previous_top && element.scrollTop !== frameTop) {
-                        resolve();
-                        return;
-                    }
-
-                    previous_top = element.scrollTop;
-
-                    setTimeout(scroll_frame, 0);
-                };
-
-                setTimeout(scroll_frame, 0);
-            });
-        };
-
-        var splitclass = this.expression.split(', ');
-        var padding = splitclass[1].replace(/\D/g, '');
-        var speed = 500;
-        var moving_frequency = 15;
-        var element;
-        splitclass[0] = splitclass[0].replace('\'', '');
-        splitclass[1] = splitclass[1].replace('\'', '');
-
-        if (element = document.querySelector(splitclass[0])) {
-
-            var hop_count = speed / moving_frequency;
-            var getScrollTopDocumentAtBegin = getScrollTopDocument();
-            var gap = (getScrollTopElement(element) - getScrollTopDocumentAtBegin - padding) / hop_count;
-
-            for (var i = 1; i <= hop_count; i++) {
-
-                (function () {
-                    var hop_top_position = gap * i;
-                    setTimeout(function () {
-                        window.scrollTo(0, hop_top_position + getScrollTopDocumentAtBegin);
-                    }, moving_frequency * i);
-                })();
-            }
+        for (let i = 0; i < events.length; i++) {
+            $element.addEventListener(events[i], handler);
+        }
+    },
+    off: function ($element, events, handler) {
+        if (!(events instanceof Array)) {
+            events = [events];
+        }
+        for (let i = 0; i < events.length; i++) {
+            $element.removeEventListener(events[i], handler);
         }
     }
-
-    Vue.directive('scroll-to', {
-
-        bind: function (el, binding) {
-            _.on(el, 'click', handleClick.bind(binding));
-        },
-
-        unbind: function (el) {
-            _.off(el, 'click', handleClick);
-        }
-    });
 };
+
+function getScrollTopElement($element) {
+    let top = 0;
+
+    while ($element.offsetParent !== undefined && $element.offsetParent != null) {
+        top += $element.offsetTop + ($element.clientTop != null ? $element.clientTop : 0);
+        $element = $element.offsetParent;
+    }
+
+    return top;
+}
+
+function getScrollTopDocument() {
+    return document.documentElement.scrollTop + document.body.scrollTop;
+}
+
+const ScrollTo = {
+    install(Vue, config) {
+        const settings = {
+            speed: 500,
+            padding: 0,
+            movingFrequency: 15
+        };
+
+        Object.assign(settings, config);
+
+        function handleClick() {
+            let options = this.expression.split(', ');
+
+            options = options.map(function (item) {
+                if (typeof item !== 'string') {
+                    return item;
+                }
+                return item.replace(/\'/g, '');
+            });
+
+            const selector = options[0];
+            const padding = (typeof options[1] === 'string')
+                ? options[1].replace(/\D/g, '')
+                : settings.padding;
+
+            const $element = _.$(selector);
+
+            if (!$element) {
+                throw new Error('$element is not defined, selector="' + selector + '" ');
+            }
+
+            const hopCount = Math.ceil(settings.speed / settings.movingFrequency);
+            const documentScrollTop = getScrollTopDocument();
+            const gap = (getScrollTopElement($element) - documentScrollTop - padding) / hopCount;
+
+            if (gap === 0) {
+                // Do not scroll when current position is the same as target offset.
+                return;
+            }
+
+            for (let i = 1; i <= hopCount; i++) {
+                (function (i) {
+                    const hopTopPosition = gap * i;
+
+                    setTimeout(function () {
+                        window.scrollTo(0, hopTopPosition + documentScrollTop);
+                    }, settings.movingFrequency * i);
+                })(i);
+            }
+        }
+
+        Vue.directive('scroll-to', {
+            bind: function ($element, context) {
+                _.on($element, 'click', handleClick.bind(context));
+            },
+
+            unbind: function ($element) {
+                _.off($element, 'click', handleClick);
+            }
+        });
+    }
+};
+
+export default ScrollTo;
